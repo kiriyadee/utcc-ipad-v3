af64d09f516f031bebfa7f6adbedf27778807a2e
Chanthawat Kiriyadee <wit.chanthawat@gmail.com>
2024-09-25 22:48:28 +0700

    Refactor signin form and page components

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -1,82 +1,30 @@
-import { redirect } from "next/navigation";
-import { signIn } from "@/lib/auth";
-import { AuthError } from "next-auth";
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
-import { Separator } from "@/components/ui/separator";
+import Image from "next/image";
+import SignInForm from "./form";
 
-export default async function SignInPage({
-  searchParams,
-}: {
-  searchParams: { error: string };
-}) {
+export default async function SignInPage() {
   return (
-    <div className="flex min-h-screen justify-center items-center">
-      <div className="flex flex-col gap-2 items-center border rounded p-5 max-w-xs">
-        <div>Sign In</div>
+    <div className="min-h-dvh flex items-center justify-center -mt-64">
+      <div className="flex-col max-w-md px-4 py-8 bg-white rounded-md shadow-lg sm:px-6 md:px-8 lg:px-10">
+        <div className="items-center justify-between pr-4 ">
+          <Image
+            src="/UTCC.png"
+            width={350}
+            height={75}
+            alt="UTCC"
+            loading="lazy"
+          />
+        </div>
+        <div className="relative h-px mt-6 bg-gray-300">
+          <div className="absolute top-0 left-0 flex justify-center w-full -mt-2">
+            <span className="px-4 text-xs text-gray-500 uppercase bg-white">
+              Login With UTCC Account
+            </span>
+          </div>
+        </div>
 
-            <form
-              key={"google"}
-              className="flex flex-col gap-2 items-center w-full"
-              action={async () => {
-                "use server";
-                try {
-                  await signIn("google", {
-                    redirectTo: "/dashboard",
-                  });
-                } catch (error) {
-                  if (error instanceof AuthError) {
-                    return redirect(`/signin/?error=${error.type}`);
-                  }
-                  throw error;
-                }
-              }}
-            >
-              <Button className="w-full" type="submit">
-                <span>Sign in with Google</span>
-              </Button>
-            </form>
-            <Separator className="my-4" />
-            <form
-              key={"credentials"}
-              className="flex flex-col gap-2 items-center w-full"
-              action={async (formData: FormData) => {
-                "use server";
-                try {
-                  await signIn("credentials", {
-                    redirectTo: "/dashboard",
-                    email: formData.get("email"),
-                    password: formData.get("password"),
-                  });
-                } catch (error) {
-                  if (error instanceof AuthError) {
-                    return redirect(`/signin/?error=${error.type}`);
-                  }
-                  throw error;
-                }
-              }}
-            >
-              <div className="w-full">
-                <Label>Email</Label>
-                <Input
-                  type="text"
-                  name="email"
-                  placeholder="shadriz@example.com"
-                />
-              </div>
-              <div className="w-full">
-                <Label>Password</Label>
-                <Input type="password" name="password" placeholder="password" />
-              </div>
-              <Button className="w-full" type="submit">
-                <span>Sign in with Credentials</span>
-              </Button>
-            </form>
-            <Separator className="my-4" />
-        {searchParams.error === "CallbackRouteError" && (
-          <div className="text-red-500">Sign in failed</div>
-        )}
+        <div className="mt-10">
+          <SignInForm />
+        </div>
       </div>
     </div>
   );
