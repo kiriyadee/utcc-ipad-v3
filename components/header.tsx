f1181930bfca19dfb0c2b01f99c164843aa848e7
Chanthawat Kiriyadee <wit.chanthawat@gmail.com>
2024-09-27 20:53:03 +0700

    Refactor layout component to remove theme provider and update header component

--- components/header.tsx
+++ components/header.tsx
@@ -2,7 +2,6 @@
 
 import * as React from "react";
 import Link from "next/link";
-import Image from "next/image";
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -12,8 +11,12 @@ import {
 import { Button } from "./ui/button";
 import { cn } from "@/lib/utils";
 import { Input } from "./ui/input";
-import { Menu, Search } from "lucide-react";
+import { LogIn, Menu, Search } from "lucide-react";
 import { ModeToggle } from "./mode-toggle";
+
+interface SignInButtonProps {
+  onSignIn: () => void;
+}
 
 export default function Header() {
   const [isScrolled, setIsScrolled] = React.useState(false);
@@ -168,5 +171,17 @@ function LanguageSelector() {
         <DropdownMenuItem>ภาษาไทย</DropdownMenuItem>
       </DropdownMenuContent>
     </DropdownMenu>
+  );
+}
+
+function SignInButton({ onSignIn }: SignInButtonProps) {
+  return (
+    <Button
+      onClick={onSignIn}
+      className="bg-gradient-to-r from-purple-500 to-indigo-600 hover:from-purple-600 hover:to-indigo-700 text-white font-semibold py-2 px-6 rounded-full shadow-lg transform transition duration-300 ease-in-out hover:scale-105 focus:outline-none focus:ring-2 focus:ring-purple-500 focus:ring-opacity-50 active:scale-95"
+    >
+      <LogIn className="w-5 h-5 mr-2" />
+      Sign In
+    </Button>
   );
 }
