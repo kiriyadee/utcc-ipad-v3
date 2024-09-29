5e4fe4ee0c3e3cc77926ca4312f8fc9bf9e45097
Chanthawat Kiriyadee <wit.chanthawat@gmail.com>
2024-09-29 14:03:09 +0700

    Refactor layout to add SpeedInsights component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import "./globals.css";
 import Footer from "@/components/footer";
 import Header from "@/components/header";
@@ -23,6 +24,7 @@ export default function RootLayout({
         <Header />
         {children}
         <Analytics />
+        <SpeedInsights />
         <Footer />
       </body>
     </html>
