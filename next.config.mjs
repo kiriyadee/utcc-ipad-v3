0ddd11116cb019e70b07ec4750cbf0810b231c67
Chanthawat Kiriyadee <wit.chanthawat@gmail.com>
2024-10-08 00:02:33 +0700

    Refactor Azure Static Web Apps workflow file and update package manager to use pnpm

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  distDir: "dist",
   output: "standalone",
   images: {
     domains: ["res.cloudinary.com", "ipad-utcc-stage.boring9.dev"],
